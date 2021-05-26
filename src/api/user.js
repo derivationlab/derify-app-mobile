c430cff4298dfd93c2a32eb4d635359ba60f25e0
Alan <770671755@qq.com>
2021-05-26 23:05:49 +0800

    526

--- src/api/user.js
+++ src/api/user.js
@@ -1,3 +1,5 @@
+// import  "@/utils/request"
+
 export function login () {
   console.log('api======login')
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -10,20 +10,57 @@
     </van-nav-bar>
           <van-popup
       v-model="show"
-      :style="{ height: '100%', width: '60%', padding: '0 0 0 1rem' }"
+      :style="{ height: '100%', width: '60%',}"
       position="left"
     >
-      <!-- <van-list router>
-        <van-cell to="/securitySettings"
-          >11</van-cell
+    <div class="headDiv">
+      <div class="imgDiv">
+        <img src="@/assets/logo.png" alt="" width="50px" height="50px">
+      </div>
+      <div class="infoDiv">
+        <span>甩哥</span>
+        <span>275fdfd39782ndfs67</span>
+      </div>
+    </div>
+            <van-list router>
+        <van-cell to="/" :border="false"
+          ><van-icon
+            color="#FFFFFF"
+            size="0.853333rem"
+            name="home-o"
+          /><span class="spSize">Home</span></van-cell
         >
-      </van-list> -->
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="exchange" /><span
+            class="spSize"
+            >Exchange</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="chart-trending-o" /><span
+            class="spSize"
+            >Dashboard</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="point-gift-o" /><span
+            class="spSize"
+            >Yield</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="friends-o" /><span
+            class="spSize"
+            >经纪商</span
+          ></van-cell
+        >
+            </van-list>
     </van-popup>
   </div>
 </template>
 
 <script>
-import { NavBar, Popup } from 'vant'
+import { NavBar, Popup, List, Cell } from 'vant'
 export default {
   name: 'Home',
   data () {
@@ -33,7 +70,9 @@ export default {
   },
   components: {
     [NavBar.name]: NavBar,
-    [Popup.name]: Popup
+    [Popup.name]: Popup,
+    [List.name]: List,
+    [Cell.name]: Cell
   },
   beforeCreate () {
     this.$store.dispatch('user/login')
@@ -45,3 +84,38 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .headDiv{
+    display: flex;
+    align-items: center;
+    margin-top: 2.5rem;
+    padding-left: 16px;
+    padding-bottom: .6rem;
+    border-bottom: 1px solid rgb(190, 190, 190);
+    .imgDiv{
+      margin-right: 16px;
+        img{
+          border-radius: 50%;
+        }
+    }
+    .infoDiv{
+      display: flex;
+      flex-direction: column;
+      line-height: 32px;
+      span:nth-child(1){
+        font-size: 16px;
+        color: rgba(255,255,255,0.85);
+      }
+       span:nth-child(2){
+        font-size: 14px;
+        color: rgba(255,255,255,0.85);
+      }
+    }
+  }
+
+  .spSize{
+      margin-left: 18px;
+      color: #FFFFFF;
+    }
+</style>
