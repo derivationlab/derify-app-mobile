8ec18305547c238d912caa18adb10f6801370d58
卢浩南 <luhaonan@weimaokeji.com>
2021-05-28 22:36:26 +0800

    refactor：extract navbar as public component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,111 @@
+<template>
+<div class="navbar-container">
+    <van-nav-bar :title="title" :border="false" :fixed="true">
+      <template #left>
+        <img  @click="ClickBox" src="@/assets/images/uil_bars.png" alt="" srcset="">
+      </template>
+      <template #title v-if="logo">
+        <img src="@/assets/images/logo.png" alt="" srcset="">
+      </template>
+    </van-nav-bar>
+          <van-popup
+      v-model="show"
+      :style="{ height: '100%', width: '60%',}"
+      position="left"
+    >
+    <div class="headDiv">
+      <div class="imgDiv">
+        <img src="@/assets/logo.png" alt="" width="50px" height="50px">
+      </div>
+      <div class="infoDiv">
+        <span>甩哥</span>
+        <span>275fdfd39782ndfs67</span>
+      </div>
+    </div>
+            <van-list router>
+        <van-cell to="/" :border="false"
+          ><van-icon
+            color="#FFFFFF"
+            size="0.853333rem"
+            name="home-o"
+          /><span class="spSize">Home</span></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" na  me="exchange" /><span
+            class="spSize"
+            >Exchange</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="chart-trending-o" /><span
+            class="spSize"
+            >Dashboard</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="point-gift-o" /><span
+            class="spSize"
+            >Yield</span
+          ></van-cell
+        >
+        <van-cell to="/" :border="false"
+          ><van-icon color="#FFFFFF" size="0.853333rem" name="friends-o" /><span
+            class="spSize"
+            >经纪商</span
+          ></van-cell
+        >
+            </van-list>
+    </van-popup>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['logo', 'title'],
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    ClickBox () {
+      this.show = true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+    .headDiv{
+    display: flex;
+    align-items: center;
+    margin-top: 2.5rem;
+    padding-left: 16px;
+    padding-bottom: .6rem;
+    border-bottom: 1px solid rgb(190, 190, 190);
+    .imgDiv{
+      margin-right: 16px;
+        img{
+          border-radius: 50%;
+        }
+    }
+    .infoDiv{
+      display: flex;
+      flex-direction: column;
+      line-height: 32px;
+      span:nth-child(1){
+        font-size: 16px;
+        color: rgba(255,255,255,0.85);
+      }
+       span:nth-child(2){
+        font-size: 14px;
+        color: rgba(255,255,255,0.85);
+      }
+    }
+  }
+
+  .spSize{
+      margin-left: 18px;
+      color: #FFFFFF;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,67 +1,17 @@
 <template>
   <div class="home">
-    <van-nav-bar title="首页" :border="false" :fixed="true">
-      <template #left>
-        <img  @click="ClickBox" src="@/assets/images/uil_bars.png" alt="" srcset="">
-      </template>
-      <template #title>
-        <img src="@/assets/images/logo.png" alt="" srcset="">
-      </template>
-    </van-nav-bar>
-          <van-popup
-      v-model="show"
-      :style="{ height: '100%', width: '60%',}"
-      position="left"
-    >
-    <div class="headDiv">
-      <div class="imgDiv">
-        <img src="@/assets/logo.png" alt="" width="50px" height="50px">
-      </div>
-      <div class="infoDiv">
-        <span>甩哥</span>
-        <span>275fdfd39782ndfs67</span>
-      </div>
-    </div>
-            <van-list router>
-        <van-cell to="/" :border="false"
-          ><van-icon
-            color="#FFFFFF"
-            size="0.853333rem"
-            name="home-o"
-          /><span class="spSize">Home</span></van-cell
-        >
-        <van-cell to="/" :border="false"
-          ><van-icon color="#FFFFFF" size="0.853333rem" name="exchange" /><span
-            class="spSize"
-            >Exchange</span
-          ></van-cell
-        >
-        <van-cell to="/" :border="false"
-          ><van-icon color="#FFFFFF" size="0.853333rem" name="chart-trending-o" /><span
-            class="spSize"
-            >Dashboard</span
-          ></van-cell
-        >
-        <van-cell to="/" :border="false"
-          ><van-icon color="#FFFFFF" size="0.853333rem" name="point-gift-o" /><span
-            class="spSize"
-            >Yield</span
-          ></van-cell
-        >
-        <van-cell to="/" :border="false"
-          ><van-icon color="#FFFFFF" size="0.853333rem" name="friends-o" /><span
-            class="spSize"
-            >经纪商</span
-          ></van-cell
-        >
-            </van-list>
-    </van-popup>
+    <navbar :logo="true" title="首页" />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
   name: 'Home',
+  components: {
+    Navbar
+  },
   data () {
     return {
       show: false
@@ -79,36 +29,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .headDiv{
-    display: flex;
-    align-items: center;
-    margin-top: 2.5rem;
-    padding-left: 16px;
-    padding-bottom: .6rem;
-    border-bottom: 1px solid rgb(190, 190, 190);
-    .imgDiv{
-      margin-right: 16px;
-        img{
-          border-radius: 50%;
-        }
-    }
-    .infoDiv{
-      display: flex;
-      flex-direction: column;
-      line-height: 32px;
-      span:nth-child(1){
-        font-size: 16px;
-        color: rgba(255,255,255,0.85);
-      }
-       span:nth-child(2){
-        font-size: 14px;
-        color: rgba(255,255,255,0.85);
-      }
-    }
-  }
-
-  .spSize{
-      margin-left: 18px;
-      color: #FFFFFF;
-    }
 </style>
