0affa6626a5e3dfb484a4f05bc407d809ff3df34
huangce.zb <huangce.zb@alibaba-inc.com>
2021-08-08 17:55:30 +0800

    open console debug log

--- src/utils/web3Utils.js
+++ src/utils/web3Utils.js
@@ -1,7 +1,7 @@
 import Contract from './contractUtil'
 import * as CfgUtil from '../config'
 
-const contractDebug = !CfgUtil.isCurrentProduction()
+const contractDebug = true
 export const EVENT_WALLET_CHANGE = 'walletChange'
 
 export function contract (account) {
