bf568cbef4ec0d3d16b1beaa4e65b4440d44b622
huangce.zb <huangce.zb@alibaba-inc.com>
2021-08-14 16:40:16 +0800

    update config files

--- src/config/index.js
+++ src/config/index.js
@@ -59,6 +59,80 @@ const config = {
         abi: DRF,
         address: '0x3A096d9d9bF3Cb355Ede5057657b887ef8C4cBed'
       },
+    },
+    debug: {
+      DerifyBond: {
+        abi: DerifyBond,
+        address: '0x3f013D2290CE40f6026B53F562630A331FA422c6'
+      },
+      DerifyDerivative: {
+        abi: DerifyDerivative,
+        BTC: {
+          address: '0xAa85ddA108AB30659a3A89E87C4048BBbAA71f54',
+          token: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+        },
+        ETH: {
+          address: '0xEf42f61000fA5d554Fd111D8d6613129b9E28f6D',
+          token: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+        }
+      },
+      DerifyExchange: {
+        abi: DerifyExchange,
+        address: '0xdBC38A5d8a10AdEb49e84e5Eb263dFa9d68a05a7'
+      },
+      DerifyStaking: {
+        abi: DerifyStaking,
+        address: '0x519231B9A4Eef0A59FE8fF57FF50fD018b9950cC'
+      },
+      DUSD: {
+        abi: DUSD,
+        address: '0x04cE9B212B43b68cfC5b67Dc0ae9bDC81727c544'
+      },
+      bDRF: {
+        abi: bDRF,
+        address: '0xDedE1D361CdD4Cfb72Eb9E41D6b530cD6581e8c8'
+      },
+      DRF: {
+        abi: DRF,
+        address: '0x3A096d9d9bF3Cb355Ede5057657b887ef8C4cBed'
+      },
+    },
+    production: {
+      DerifyBond: {
+        abi: DerifyBond,
+        address: '0x3f013D2290CE40f6026B53F562630A331FA422c6'
+      },
+      DerifyDerivative: {
+        abi: DerifyDerivative,
+        BTC: {
+          address: '0xAa85ddA108AB30659a3A89E87C4048BBbAA71f54',
+          token: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+        },
+        ETH: {
+          address: '0xEf42f61000fA5d554Fd111D8d6613129b9E28f6D',
+          token: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+        }
+      },
+      DerifyExchange: {
+        abi: DerifyExchange,
+        address: '0xdBC38A5d8a10AdEb49e84e5Eb263dFa9d68a05a7'
+      },
+      DerifyStaking: {
+        abi: DerifyStaking,
+        address: '0x519231B9A4Eef0A59FE8fF57FF50fD018b9950cC'
+      },
+      DUSD: {
+        abi: DUSD,
+        address: '0x04cE9B212B43b68cfC5b67Dc0ae9bDC81727c544'
+      },
+      bDRF: {
+        abi: bDRF,
+        address: '0xDedE1D361CdD4Cfb72Eb9E41D6b530cD6581e8c8'
+      },
+      DRF: {
+        abi: DRF,
+        address: '0x3A096d9d9bF3Cb355Ede5057657b887ef8C4cBed'
+      },
     }
   }
 }
