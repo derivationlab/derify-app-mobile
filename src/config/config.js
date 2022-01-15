7a2132e2d5d4b2d81ddbddb4ab46c9222e737655
huangce.zb <huangce.zb@alibaba-inc.com>
2022-01-15 16:42:35 +0800

    switch network

--- src/config/config.js
+++ src/config/config.js
@@ -163,7 +163,7 @@ export default {
           "abi": DerifyDerivative,
           "BTC": {
             "address": "0x7F4c1429a99b16df381430818F826a00Ae463578",
-            "token": "0xaC413A0B3ce02D8c9D13a697A14D8FB01cFD9A9e"
+            "token": "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
           },
           "ETH": {
             "address": "0xaC413A0B3ce02D8c9D13a697A14D8FB01cFD9A9e",
