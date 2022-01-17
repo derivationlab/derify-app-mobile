31b237bec5fa963f6a36f8301ccf7213199d772e
huangce.zb <huangce.zb@alibaba-inc.com>
2022-01-17 23:57:08 +0800

    update mul-chain server url

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,13 +3,13 @@
  */
 import axios from 'axios'
 import QS from 'qs'
-import { Toast } from 'vant'
+
 import router from '../router/index'
 import store from '../store/index'
-import cfg, { getCurrentServerEndPoint } from '../config'
+import cfg, {getCurrentServerEndPoint} from '../config'
 
 // Environment switch
-axios.defaults.baseURL = getCurrentServerEndPoint();
+//axios.defaults.baseURL = getCurrentServerEndPoint();
 
 // Request timeout
 axios.defaults.timeout = 60000
@@ -22,8 +22,6 @@ axios.interceptors.request.use(
   config => {
     // Before each request is sent, determine whether there is a token. If it exists, the token will be added to the header of the http request. There is no need to manually add it for each request.
     // Even if there is a token locally, it is possible that the token is expired, so the return status should be judged in the response interceptor
-    const token = store.state.token
-    token && (config.headers.Authorization = token)
     return config
   },
   error => {
@@ -41,7 +39,58 @@ axios.interceptors.response.use(
   },
   // When the server status code is not 200
   error => {
+    console.error(`axios onReject ${error}`)
+
     if (error.response.status) {
+      switch (error.response.status) {
+        // 401: Not logged in
+        // Jump to the login page if you are not logged in, and carry the path of the current page
+        // Return to the current page after successful login, this step needs to be operated on the login page.
+        case 401:
+          router.replace({
+            path: '/login',
+            query: { redirect: router.currentRoute.fullPath }
+          })
+          break
+          // 403 token expired
+          // Prompt the user after login expiration
+          // Clear the local token and empty the token object in vuex
+          // Jump to login page
+        case 403:
+          // Toast({
+          //   message: 'Login expired, please log in again\n',
+          //   duration: 1000,
+          //   forbidClick: true
+          // })
+          // Clear token
+          // localStorage.removeItem('token')
+          // store.commit('loginSuccess', null)
+          // Jump to the login page, and pass the fullPath of the page to be browsed, after successful login, jump to the page that needs to be visited
+          // setTimeout(() => {
+          //   router.replace({
+          //     path: '/login',
+          //     query: {
+          //       redirect: router.currentRoute.fullPath
+          //     }
+          //   })
+          // }, 1000)
+          break
+          // 404 request does not exist
+        case 404:
+          // Toast({
+          //   message: 'Network request does not exist\n',
+          //   duration: 1500,
+          //   forbidClick: true
+          // })
+          break
+          // Other errors, throw an error prompt directly
+        default:
+          // Toast({
+          //   message: error.response.data.message,
+          //   duration: 1500,
+          //   forbidClick: true
+          // })
+      }
       return Promise.reject(error.response)
     }
   }
@@ -53,13 +102,14 @@ axios.interceptors.response.use(
   */
 export function get (url, params) {
   return new Promise((resolve, reject) => {
-    axios.get(url, {
+    axios.get(getUrlWithBase(url), {
       params: params
     })
       .then(res => {
         resolve(res.data)
       })
       .catch(err => {
+        console.error(`axios get ${err}`)
         reject(err.data)
       })
   })
@@ -71,10 +121,9 @@ export function get (url, params) {
  * @param config
   */
 export function post (url, params, config = null) {
-
   return new Promise((resolve, reject) => {
     if(config) {
-      axios.post(url, params, config)
+      axios.post(getUrlWithBase(url), params, config)
         .then(res => {
           resolve(res.data)
         })
@@ -91,4 +140,13 @@ export function post (url, params, config = null) {
         })
     }
   })
+}
+
+
+function getUrlWithBase(url){
+  if(url.test(/^https?:\/\/.*/)){
+    return url;
+  }else{
+    return getCurrentServerEndPoint() + url;
+  }
 }
