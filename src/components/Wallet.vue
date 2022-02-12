d738c56925a35a432e7ecefdcb3b4f6296541178
huangce.zb <huangce.zb@alibaba-inc.com>
2022-02-12 13:12:28 +0800

    fix wallet connet error msg

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div class="error-notice" v-if="showNetworkError">
-      <span>{{$t('Trade.Wallet.MainChainUnmatch', [mainChain.name])}}</span>
+      <span>{{$t('Trade.Wallet.MainChainUnmatch', [selectedWalletNetwork.name])}}</span>
       <div class="error-right">
         <van-icon name="cross" class="van-icon-close" color="#EA446B" @click="()=>{this.showNetworkError=false}"></van-icon>
       </div>
